2106395f2d09332fe230a4a0feefeedaa6dfb374
wwagner4 <wwagner4@gmail.com>
2022-01-30 08:03:34 +0100

    show image

---
+++ vue1/project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: lightblue;
+  background-color: #294445;
+  margin-top: 4em;
+  font-size: xx-large;
+}
+
+</style>
---
+++ vue1/project/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h3>create the following diagram in your language</h3>
+    <img alt="intro image" :src="'/intro.svg'" width="1300">
+    <div class="content">
+      <table>
+        <tr>
+          <td>x label</td>
+          <td><input type="text" name="xlabel"></td>
+        </tr>
+        <tr>
+          <td>y label</td>
+          <td><input type="text" name="ylabel"></td>
+        </tr>
+        <tr>
+          <td>z label</td>
+          <td><input type="text" name="zlabel"></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <a href="#">create</a>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  margin-left: auto;
+  margin-right: auto;
+  width: fit-content;
+}
+</style>
